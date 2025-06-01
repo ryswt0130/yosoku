8f55bee5159fbcb9a09c5c46c52f8030b37bedf7
ryswt0130 <37707147+ryswt0130@users.noreply.github.com>
2025-06-02 00:25:12 +0900

    Delete toppage directory

--- toppage/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Product_line, Product_series, User_yosoku_date, Post
-
-admin.site.register([Product_line, Product_series, User_yosoku_date, Post])
--- toppage/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ToppageConfig(AppConfig):
-    name = 'toppage'
